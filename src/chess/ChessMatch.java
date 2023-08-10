5dfb94c14210301944f7774fe9438cbb48d0cf91
Luziano Farias <luziano-farias1993@hotmail.com>
2023-08-09 23:45:52 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -40,7 +40,13 @@ public class ChessMatch {
         return capturedPiece;
     }
 
-    private void validateSourcePosition(Position source) {
+    private void validateSourcePosition(Position position) {
+        if (!board.thereIsAPiece(position)) {
+            throw new ChessException("There is no piece on source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There is no possible moves for the chosen pieces");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
